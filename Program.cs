d6bffbd4e3dbb7ba3c7230ba500d8280246e4b33
Admin <Admin@WIN-0QFN66U68EP>
2024-12-17 06:09:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст:");
+        string input = Console.ReadLine();
+        string[] words = input.Split(new char[] { ' ', ',', '.', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+        Console.WriteLine("Слова, начинающиеся с большой буквы и заканчивающиеся на две цифры:");
+
+        foreach (var word in words)
+        {
+            if (IsValidWord(word))
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+
+    static bool IsValidWord(string word)
+    {
+        // Проверка на начало с большой буквы и наличие двух цифр в конце
+        if (word.Length < 3) return false; // Слово должно иметь хотя бы 3 символа
+        if (char.IsUpper(word[0]) && char.IsDigit(word[word.Length - 1]) && char.IsDigit(word[word.Length - 2]))
+        {
+            // Проверяем, что перед последними двумя цифрами нет других цифр
+            for (int i = 0; i < word.Length - 2; i++)
+            {
+                if (char.IsDigit(word[i]))
+                {
+                    return false; // Если есть цифры перед последними двумя, то слово не подходит
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
